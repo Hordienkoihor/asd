0ba174ebe43e5a340a9f191fbdcc24d03aae6510
Hordienkoihor <hordienkoihor05@gmail.com>
2026-02-10 09:16:00 +0200

    extended method send

--- src/main/java/practice5/assignment3/MultiNotifier.java
+++ src/main/java/practice5/assignment3/MultiNotifier.java
@@ -3,6 +3,7 @@ package practice5.assignment3;
 public class MultiNotifier implements EmailNotifier, SmsNotifier {
     @Override
     public void send(String message) {
+        System.out.println("Calling super methods: ");
         EmailNotifier.super.send(message);
         SmsNotifier.super.send(message);
     }
